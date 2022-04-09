d0dad3aaaa110d7f3106f20e9882f6ca5ce912dd
Mohammadreza Ghofrani <ghofrani132@gmail.com>
2022-04-09 22:16:00 +0430

    Do NN HW3
    Closes #26

--- 2nd_semester/nn/src/slides/slide_fetcher.bash
+++ 2nd_semester/nn/src/slides/slide_fetcher.bash
@@ -1,9 +1,10 @@
 name="hello"
 server_id=10
 class_id=29b46525686ccf97ef3f12bf825c56733229efd5-1648960891613
-session_id=438d148bfa3cf13739c950d1a002cf83e865aa96-1648960980166
+session_id=8b28fc6919bf89a89bae56b971046bb389a0041a-1650775690554
 url="https://blue$server_id.aut.ac.ir/bigbluebutton/presentation/$class_id/$class_id/$session_id/svg"
 tmp_dir=$(mktemp -d)
+echo $url
 for i in {1..69}
 do
 	if ! curl -s --head  --request GET "$url/$i" | grep "404 Not Found" > /dev/null
